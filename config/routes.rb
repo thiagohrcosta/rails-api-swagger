a8be4f2ccb74ff85011cead7bf3b635743765981
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-04 20:11:41 -0300

    Install Swagger - RSWAG GEM

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
